82424642b828ee9b3334906aacb69d6b1de87f52
mah752 <83982869+mah752@users.noreply.github.com>
2021-12-05 21:02:52 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -79,33 +79,33 @@ function searchCity(city) {
     axios.get(apiUrl).then(displayWeather);
     console.log(apiUrl);
 }
+
 //search form
 function handleSubmit(event) {
     event.preventDefault();
     let cityInput = document.querySelector("#search-input").value;
     searchCity(cityInput);
 }
-
-//submitting the form
-let inputForm = document.querySelector("#search-form");
-inputForm.addEventListener("submit", handleSubmit);
 //This city will show by default
 searchCity("London");
+
+let inputForm = document.querySelector("#search-form");
+inputForm.addEventListener("submit", handleSubmit);
+
 //current temperature
 function displayWeather(response) {
     let weatherDiv = document.querySelector("#temp-input");
     let temperature = Math.round(response.data.main.temp);
     weatherDiv.innerHTML = `${temperature}°C`;
-}
-//fahrenheit temperature
-function displayFarenheitTemperature(event) {
-    event.preventDefault();
-    let temperatureElement = document.querySelector("#temp-input");
-    let fahrenheittemp = (temperature * 9) / 5 + 32;
-    temperatureElement.innerHTML = Math.round(
-        fahrenheittemp
-    );
-    let fahrenheitLink = document.querySelector(".faranheit");
+
+    //fahrenheit temperature
+    function displayFarenheitTemperature(event) {
+        event.preventDefault();
+        let temperatureElement = document.querySelector("#temp-input");
+        let fahrenheittemp = (temperature * 9) / 5 + 32;
+        temperatureElement.innerHTML = Math.round(fahrenheittemp);
+    }
+    let fahrenheitLink = document.querySelector("#farenheit-click");
     fahrenheitLink.addEventListener("click", displayFarenheitTemperature);
 
     function displayCelciustemp(event) {
@@ -114,25 +114,31 @@ function displayFarenheitTemperature(event) {
         temperatureElement.innerHTML = displayWeather;
     }
     let celcuistemp = null;
-
-    let celciusLink = document.querySelector(".celcius");
+    //submitting the form
+    let inputForm = document.querySelector("#search-form");
+    inputForm.addEventListener("submit", handleSubmit);
+    //This city will show by default
+    searchCity("London");
+    let celciusLink = document.querySelector("#celcius-click");
     celciusLink.addEventListener("click", displayCelciustemp);
     //current city
     let currentCity = response.data.name;
     let displaycurrentCity = document.querySelector("#city-input");
     displaycurrentCity.innerHTML = `${currentCity}`;
+
     //humidity
     let humidity = response.data.main.humidity;
     let humidityInput = document.querySelector("#humidity-input");
     humidityInput.innerHTML = `${humidity}%`;
-
     let windSpeed = Math.round(response.data.wind.speed);
     let windSpeedInfo = document.querySelector("#wind-input");
     windSpeedInfo.innerHTML = `${windSpeed} km/h`;
+
     //Temperature for feels like input 
     let feelsLike = Math.round(response.data.main.feels_like);
     let feelsInput = document.querySelector("#feels-input");
     feelsInput.innerHTML = `${feelsLike}°C`;
+
     //forecast description
     let currentForecastNow = response.data.weather[0].description;
     let forecastDescriptionnow = document.querySelector("#forecast-input");
@@ -162,5 +168,3 @@ function getCurrentLocation(event) {
     event.preventDefault();
     navigator.geolocation.getCurrentPosition(showPosition);
 }
-let CurrentWeatherbyLocation = document.querySelector("#current-location");
-CurrentWeatherbyLocation.addEventListener("click", getCurrentLocation);
