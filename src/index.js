e704f8382f9967c9fb9c1dd1cfa94245d137ace3
mah752 <83982869+mah752@users.noreply.github.com>
2021-12-13 13:15:24 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -167,3 +167,19 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-click");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+//responsive screensize
+$(document).ready(function() {
+    $(window).bind("resize", resizeWindow);
+
+    function resizeWindow(e) {
+        var newWindowWidth = $(window).width();
+
+        // If width width is below 600px, switch to the mobile stylesheet
+        if (newWindowWidth < 600) { $("link[rel=stylesheet]").attr({ href: "mobile.css" }); }
+        // Else if width is above 600px, switch to the large stylesheet       
+        else if (newWindowWidth > 600) {
+            $("link[rel=stylesheet]").attr({ href: "style.css" });
+        }
+    }
+});
